df00bb0cb30059affb6c839bf1f0d306c2c7ffe9
Kariny Heloiza Lauxen <karinylauxenif2024@gmail.com>
2025-05-26 15:25:22 -0400

    Add files via upload

---
+++ listadecompras.py
@@ -0,0 +1,22 @@
+# lista = []
+# sair= 'nao'
+
+# while sair == 'nao':
+#     print("Deseja adicionar um item na lista?")
+#     resposta1 = input("sim ou nao")
+#     if resposta1 == "sim":
+#         item = input('digite o nome do item a ser adicionado')
+#         lista.append(item)
+#         print (" lista atual:", lista)
+#     else:
+#         print (" deseja excluir um item da lista?")
+#         resposta2 = input (" sim ou nao:")
+#         if resposta2 == "sim":
+#             item = input("digite o nome do item a ser removido:")
+#             if item in lista:
+#              lista.remove (item)
+#             print ('lista atual:', lista)
+#         else:
+#             print ("deseja finalizar?")
+#             sair= input ("sim ou nao")
+# print ("programa finalizado. lista final:", lista)
---
+++ testando_definicoes.py
@@ -0,0 +1,32 @@
+# compras = ["pão", "café", "leite"]
+# print(compras)
+
+# compras.remove(compras[0])
+# print(compras)
+
+# compras.append("açucar")
+# print(compras)
+
+# compras_ordenada = sorted(compras)
+# print(compras_ordenada)
+
+# for item in compras:
+#       print ("-", item)
+#o nome item é um identificador dos itens da lista
+#listas= []
+#dicionarios = {chave: valor}
+#tuplas= ()
+#conjuntos = set ()
+
+#uma tupla
+#teste= [1,2,3,5]
+#print(teste)
+#teste.append(4)
+#print(teste)
+
+#um conjunto
+teste={1,2,3,5,5,6,6}
+print(teste)
+# teste.remove(1)
+#print(teste)
+#teste.append("1")
